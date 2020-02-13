f19b1cd5e74a3f0933b7978a14e253c79e55b789
YohannaBordon25 <60674877+YohannaBordon25@users.noreply.github.com>
2020-02-12 22:17:01 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,45 @@
-function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+function mostrar() {
+    //tomo la edad  
+
+    var mes = document.getElementById('mes').value;
+
+    switch (mes) {
+
+        case "Enero": 
+
+            alert("que comiences bien el año")
+    
+            break;
+        
+
+
+        case
+            "Marzo":
+
+            alert("a clases!!");
+            break;
+
+
+
+        case
+            "Julio":
+
+            alert("Se vienen las vacaciones");
+            break;
+
+
+        case "Diciembre":
+
+            alert("Felices fiestas")
+            break;
+
+        default:
+            alert("A ese mes no lo tuve en cuenta");
+
+
+    }
 
 
 
 
-}//FIN DE LA FUNCIÓN
+    }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,7 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
+
+
+switch  (mes){
+
+    case "Febrero":
+    alert ("Este mes no tiene mas de 29 dias");
+    break;
+
+
+    default: 
+    alert ("Este mes tiene 30 o mas dias"); 
+    
+
+}
+
+
+
+
+
+
 
 //alert (mesDelAño);
 	
